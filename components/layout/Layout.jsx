49332f4fd0c4aedb042b824b263c86d54bdab3c6
Sebastian Rachfał <rachfalsebastian@gmail.com>
2021-03-14 21:38:31 +0100

    visual adjustments

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Head from 'next/head';
 
 import { selectCart, setCart } from '../../lib/slices/userSlice';
 
@@ -25,6 +26,10 @@ export default function Layout({ children }) {
 
 	return (
 		<div className='flex flex-col h-screen'>
+			<Head>
+				<title>Store</title>
+				<meta property='og:title' content='Store' key='title' />
+			</Head>
 			<Header />
 			<main className='container sm:px-0 w-full lg:px-6 mx-auto text-center bg-white rounded-xl pt-20 min-h-full'>
 				{children}
